c0652792efedc47103cfcbba43a941736af6e501
Yulian Matev <30836371+ymatev@users.noreply.github.com>
2023-11-14 23:16:55 +0200

    Make unit test fail

--- app/app_test.py
+++ app/app_test.py
@@ -19,7 +19,7 @@ class TestApp(unittest.TestCase):
         """Testing hello world"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Hello, World!")
+        self.assertEqual(response.data, b"Hello, World! error")
 
 
 if __name__ == "__main__":

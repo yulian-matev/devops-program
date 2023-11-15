139d11463a8c093e9cdf145f1b49f3d585cf7597
Yulian Matev <30836371+ymatev@users.noreply.github.com>
2023-11-15 11:27:18 +0200

    Reformat with black

--- app/app.py
+++ app/app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
-
 def hello_world():
     """Function hello_world"""
     return "Hello, World!"
--- app/app_test.py
+++ app/app_test.py
@@ -4,13 +4,11 @@ import unittest
 from app import app
 
 
-
 class TestApp(unittest.TestCase):
     """My very importatn TestApp"""
 
     def setUp(self):
         self.client = app.test_client()
-
 
     def test_hello_world(self):
         """Testing hello world"""
@@ -20,6 +18,6 @@ class TestApp(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    #suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestApp)
-    #unittest.TextTestRunner().run(suite)
+    # suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestApp)
+    # unittest.TextTestRunner().run(suite)
     unittest.main()
